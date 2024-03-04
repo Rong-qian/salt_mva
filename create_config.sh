64344139be120401d17bcdff0cf51af9355ff163
hlin <hui-chi.lin@cern.ch>
2024-03-04 04:58:58 -0500

    update ttHH configs

--- create_config.sh
+++ create_config.sh
@@ -3,26 +3,26 @@
 BTAGNAME=(Legacy77)
 nBTagsVar=(nBTags_GN2v00LegacyWP_77)
 
-# folder=GN2_ttZp_all_2class_SR_edge
-
-# for btagIdx in {0..0..1}
-# do
-#     subfolder=${folder}/GNT_ttZp_mZp1000_${BTAGNAME}_SR_2class_edge_v04_00_0001_v3
-#     mkdir -p salt/config/${subfolder}
-#     cp salt/configs/${folder}/template.yaml salt/configs/${subfolder}/template.yaml
-#     sed -i "s/BTAGNAME/${BTAGNAME[$btagIdx]}/g" salt/configs/${subfolder}/template.yaml
-#     sed -i "s/nBTagsVar/${nBTagsVar[$btagIdx]}/g" salt/configs/${subfolder}/template.yaml
-#     source create_config_per_btag.sh $subfolder
-# done
-
-folder=GN2_ttHH_all_2class_SR_edge
+folder=GN2_ttZp_all_2class_SR_edge
 
 for btagIdx in {0..0..1}
 do
-    subfolder=${folder}/GNT_ttHH_${BTAGNAME}_SR_2class_edge_v04_00_0001_v2
-    mkdir -p salt/configs/$subfolder
+    subfolder=${folder}/GNT_ttZp_mZp1000_${BTAGNAME}_SR_2class_edge_v04_00_0001_v3
+    mkdir -p salt/configs/${subfolder}
     cp salt/configs/${folder}/template.yaml salt/configs/${subfolder}/template.yaml
     sed -i "s/BTAGNAME/${BTAGNAME[$btagIdx]}/g" salt/configs/${subfolder}/template.yaml
     sed -i "s/nBTagsVar/${nBTagsVar[$btagIdx]}/g" salt/configs/${subfolder}/template.yaml
     source create_config_per_btag.sh $subfolder
 done
+
+# folder=GN2_ttHH_all_2class_SR_edge
+
+# for btagIdx in {0..0..1}
+# do
+#     subfolder=${folder}/GNT_ttHH_${BTAGNAME}_SR_2class_edge_v04_00_0001_v2
+#     mkdir -p salt/configs/$subfolder
+#     cp salt/configs/${folder}/template.yaml salt/configs/${subfolder}/template.yaml
+#     sed -i "s/BTAGNAME/${BTAGNAME[$btagIdx]}/g" salt/configs/${subfolder}/template.yaml
+#     sed -i "s/nBTagsVar/${nBTagsVar[$btagIdx]}/g" salt/configs/${subfolder}/template.yaml
+#     source create_config_per_btag.sh $subfolder
+# done
