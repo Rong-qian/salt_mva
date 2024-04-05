2466e69d8ca8f1544af7b8cf4a38ee27c7f84e56
hlin <hui-chi.lin@cern.ch>
2024-04-05 03:57:25 -0400

    multiclass training

--- salt/utils/class_names.py
+++ salt/utils/class_names.py
@@ -17,4 +17,56 @@ CLASS_NAMES = {
         "Electron",
         "Muon",
     ],
+    "flavour_label":[
+		"tttt",
+        "ttt",
+        "ttV",
+        "otherbkg",
+    ],
+    "truthflav":[
+        "NonJet",
+        "light",
+        "charm",
+        "bottom",
+        "tau",
+    ],
+    "truth_label":[
+        # "NonMatch",
+        "Top",
+        "Higgs",
+        "Z",
+    ],
+    "truth_vertex":[
+        "NonMatch",
+        "First",
+        "Second",
+        "Third",
+        "Fourth",
+    ],
+    # "originIndexZp":[
+    #     "NonMatch",
+    #     "FirstZp",
+    #     "SecondZp",
+    #     "ThirdZp",
+    #     "FourthZp",
+    # ],
+    # "originIndexTop":[
+    #     "NonMatch",
+    #     "FirstTop",
+    #     "SecondTop",
+    #     "ThirdTop",
+    #     "FourthTop",
+    # ],
+    # "originIndexW":[
+    #     "NonMatch",
+    #     "FirstW",
+    #     "SecondW",
+    #     "ThirdW",
+    #     "FourthW",
+    # ],
+
+  
+    
+
+
 }
