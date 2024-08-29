e852b459014f59f096ed3788eadde84ab5c056b8
hlin <hui-chi.lin@cern.ch>
2024-08-29 05:53:21 -0400

    clean up

--- scripts/script_tttt.sh
+++ scripts/script_tttt.sh
@@ -1,95 +1,19 @@
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3_fold0.txt 2>&1
-
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3/fold1.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3_fold1.txt 2>&1
-
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3/fold2.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3_fold2.txt 2>&1
-
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3/fold3.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3_fold3.txt 2>&1
-
-
-## testing
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v1p1/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v1p1_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v1p2/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v1p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v1p3/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v1p3_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3p1/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3p1_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3p2/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v3p3/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v3p3_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v4/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v4_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v4p1/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v4p1_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v4p2/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v4p2_fold0.txt 2>&1
-
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v4p3/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v4p3_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_Legacy77_v4p4/fold0.yaml --force > logs/tttt_all_4class_SR_Legacy77_v4p4_fold0.txt 2>&1
-
-# # TopCPToolkit samples (no EDGE features, no auxiliary tasks)
-
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p2/fold0.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p3/fold0.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p3_fold0.txt 2>&1
-
-# default (b-jet as auxiliary task) ###
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p4/fold0.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p4_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p4/fold1.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p4_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p4/fold2.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p4_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p4/fold3.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p4_fold3.txt 2>&1
-
-# v4p5 sample v2
+# === tttt ===
+# v4p5
 salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p5/fold0.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p5_fold0.txt 2>&1
 salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p5/fold1.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p5_fold1.txt 2>&1
 salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p5/fold2.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p5_fold2.txt 2>&1
 salt fit --config /atlas/data19/huichil/salt/salt/configs/tttt_all_4class_SR/tttt_all_4class_SR_GN2v01WP85_v4p5/fold3.yaml --force > logs/tttt_all_4class_SR_GN2v01WP85_v4p5_fold3.txt 2>&1
 
-# ttZp
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4/fold0.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4/fold1.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4/fold2.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4/fold3.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4_fold3.txt 2>&1
-
-# ttZp: try increase the weight
-# 10%
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4p2/fold0.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4p2/fold1.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4p2_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4p2/fold2.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4p2_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4p2/fold3.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4p2_fold3.txt 2>&1
-# 20%
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_all_4class_SR_GN2v01WP85_v4p4p3/fold0.yaml --force > logs/ttZp_all_4class_SR_GN2v01WP85_v4p4p3_fold0.txt 2>&1
-
-# ttZp_mZp2000 v4p4p2
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2/fold0.yaml --force > logs/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2/fold1.yaml --force > logs/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2/fold2.yaml --force > logs/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2/fold3.yaml --force > logs/ttZp_mZp2000_all_4class_SR_GN2v01WP85_v4p4p2_fold3.txt 2>&1
-
-# ttZp_mZp3000 v4p4p2
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2/fold0.yaml --force > logs/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2/fold1.yaml --force > logs/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2/fold2.yaml --force > logs/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2/fold3.yaml --force > logs/ttZp_mZp3000_all_4class_SR_GN2v01WP85_v4p4p2_fold3.txt 2>&1
-
-# ttZp allmass v4p4p2
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2/fold0.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2/fold1.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2/fold2.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2/fold3.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p4p2_fold3.txt 2>&1
-
-# ttZp allmass 2class v4p4p2
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_2class_SR/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2/fold0.yaml --force > logs/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2_fold0.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_2class_SR/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2/fold1.yaml --force > logs/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2_fold1.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_2class_SR/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2/fold2.yaml --force > logs/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2_fold2.txt 2>&1
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_2class_SR/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2/fold3.yaml --force > logs/ttZp_allmass_2class_SR_GN2v01WP85_v4p4p2_fold3.txt 2>&1
-
-# v4p5: change the number of layers and nodes
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p5/fold0.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p5_fold0.txt 2>&1 &
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p5/fold1.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p5_fold1.txt 2>&1 &
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p5/fold2.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p5_fold2.txt 2>&1 &
-salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p5/fold3.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p5_fold3.txt 2>&1 &
-
+# === ttZp ===
 # v4p6: mass parameterized
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p6/fold0.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p6_fold0.txt 2>&1 &
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p6/fold1.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p6_fold1.txt 2>&1 &
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p6/fold2.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p6_fold2.txt 2>&1 &
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttZp_all_4class_SR/ttZp_allmass_4class_SR_GN2v01WP85_v4p6/fold3.yaml --force > logs/ttZp_allmass_4class_SR_GN2v01WP85_v4p6_fold3.txt 2>&1 &
 
-# ttH v4p6
+# === ttH === 
+# v4p6
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttH_all_4class_SR/ttH_allmass_4class_SR_GN2v01WP85_v4p6/fold0.yaml --force > logs/ttH_allmass_4class_SR_GN2v01WP85_v4p6_fold0.txt 2>&1 &
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttH_all_4class_SR/ttH_allmass_4class_SR_GN2v01WP85_v4p6/fold1.yaml --force > logs/ttH_allmass_4class_SR_GN2v01WP85_v4p6_fold1.txt 2>&1 &
 salt fit --config /atlas/data19/huichil/salt/salt/configs/ttH_all_4class_SR/ttH_allmass_4class_SR_GN2v01WP85_v4p6/fold2.yaml --force > logs/ttH_allmass_4class_SR_GN2v01WP85_v4p6_fold2.txt 2>&1 &
